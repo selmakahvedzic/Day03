0ac8eedc80e0c9daf2cae492e5306a8ebaf241c1
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-10 14:38:57 +0100

    ex04 done!

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    //Only change code below this line
+    var x, y;
+    x = 10; 
+    y = x; 
+    return [x,y];
+    //Only change code above this line
+  
+ }
+ 
+ console.log(main());
+ 
+ module.exports = main;
