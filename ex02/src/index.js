5ee473e6477c1546d6709564de7286d99a41ed48
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-10 14:18:50 +0100

    ex02 done !

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+   //This is single-line JavaScript comment
+
+   /*This
+   is
+   multi-lines
+   JavaScript
+   comment*/
+ 
+}
+
+console.log(main());
+module.exports = main;
